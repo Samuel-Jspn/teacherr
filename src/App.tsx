a8969bf63bd91e924dece6fc44885f28c4ba97d7
sam.josephine@outlook.fr <sam.josephine@outlook.fr>
2023-06-07 17:22:55 +0200

    formatted + add colors
    Formatted code and added the project colors to index.css AND app.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/home"
-import Connection from './pages/connection'
-import Inscription from './pages/inscription'
-
+import Home from "./pages/home";
+import Connection from "./pages/connection";
+import Inscription from "./pages/inscription";
 
 function App() {
-  
   return (
     <body>
-      <header>
-      </header>
+      <header></header>
 
       <div className="Teacherr">
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/connection' element={<Connection />} />
-          <Route path='/inscription' element={<Inscription />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/connection" element={<Connection />} />
+          <Route path="/inscription" element={<Inscription />} />
         </Routes>
       </div>
     </body>
