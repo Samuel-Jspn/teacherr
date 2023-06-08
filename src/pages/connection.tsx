383c142df2621c1187fd1daf49de1692e6eb8a88
sam.josephine@outlook.fr <sam.josephine@outlook.fr>
2023-06-08 10:53:49 +0200

    Fix css + try component header
    Fix css for connexion and inscription. Classname changed, files and folders moved for component development purposes.
    Header component WIP

--- src/pages/connection.tsx
+++ src/pages/connection.tsx
@@ -1,25 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/connection/connection.css";
+import "boxicons";
 
 function connectionPage() {
   return (
     <body>
       <div className="all">
         <form action="" method="post" className="">
-          <h1 className="titre">Connexion</h1>
-          <div className="ChampRenseignemants">
+          <h1 className="cTitre">Connexion</h1>
+          <div className="cChampRenseignements">
             <input type="email" placeholder="Adresse email" />
             <i className="bx bxs-envelope"></i>
           </div>
-          <div className="ChampRenseignemants">
+          <div className="cChampRenseignements">
             <input type="password" placeholder="Mot de passe" />
             <i className="bx bxs-lock-alt"></i>
           </div>
           <button className="connection">
             <div className="connexion">
               <p>Je me connecte</p>
-              <i className="bx bx-right-arrow-alt"></i>
+              <i className="bx bx-right-arrow-alt" />
             </div>
           </button>
           <Link to="/inscription">
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,16 @@
 import React from "react";
+import Title from "./components/title/title.jsx";
+import "../styles/home/home.css";
+import teacherrLogo from "./assets/teacherrLogo.png";
 
-import '../styles/home/home.css';
-
-function home() {   
-    return (
-        <body>
-        
-        </body>
-    );
+function home() {
+  return (
+    <body>
+      {/* <Title /> */}
+      <img src={teacherrLogo} />
+      <h1>Teacherr</h1>
+    </body>
+  );
 }
 
 export default home;
