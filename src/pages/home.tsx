a05698c7e880a76465331cc2f18eda9dab80a1ad
MathildeLepers <94354160+MathildeLepers@users.noreply.github.com>
2023-06-08 10:34:49 +0000

    block prof

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,28 @@
 import React from "react";
-
+import {Link} from "react-router-dom"
 import '../styles/home/home.css';
 
 function home() {   
     return (
         <body>
-        
+            <div className="all">
+                <div className='block'>
+                    <form action="" method="post" className="">
+                        <h1 className="title">Professeur en Maths</h1>
+                            <div className='champ'>
+                                Niveau Bac+2
+                            </div>
+                            <div className='champ'>
+                                Description de la méthode de travail, des chapitres... 
+                            </div>
+                            <button className='button'>
+                            <div className='rdv'>
+                                <p>Prendre<br /> rendez-vous</p>
+                            </div>
+                        </button>
+                    </form>
+                   </div>
+               </div>
         </body>
     );
 }
