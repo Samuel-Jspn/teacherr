d9d40db877e50bbd76855e179394f9bab7e4056c
KraX <louis.delattre@viacesi.fr>
2023-06-09 15:24:26 +0200

    Ajout du back

---
+++ back/routes/authRoutes.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+router.post('/register', async (req, res) => {
+  try {
+    const { nom, prenom, email, password } = req.body;
+
+    // Vérifier si l'utilisateur existe déjà dans la base de données
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
+    // Hacher le mot de passe avant de le sauvegarder dans la base de données
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
+      nom,
+      prenom,
+      email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to register user' });
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Vérifier si l'utilisateur existe dans la base de données
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    // Comparer les mots de passe hachés
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    // Créer et renvoyer un jeton d'authentification
+    const token = jwt.sign({ userId: user._id }, 'your-secret-key', { expiresIn: '1h' });
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to authenticate' });
+  }
+});
+
+module.exports = router;
