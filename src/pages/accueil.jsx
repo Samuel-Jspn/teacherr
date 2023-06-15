94792e86af03fe6571843de43f79e61e0eabc7a1
sam.josephine@outlook.fr <sam.josephine@outlook.fr>
2023-06-15 11:34:37 +0200

    added landing page
    Created landing page

--- src/pages/accueil.jsx
+++ src/pages/accueil.jsx
@@ -1,12 +1,32 @@
 import React from "react";
 import "../styles/accueil/accueil.css";
-import Navbar from "./components/navbar/navbar.jsx";
+import { Link } from "react-router-dom";
+// import teacherr from "../../assets/teacherr.png";
+import teacherr from "./assets/teacherr.png";
+import teacherrLogo from "./assets/teacherrLogo.png";
 
 function home() {
   return (
     <body>
-      <div>
-        <Navbar />
+      <div className="preview">
+        <img className="preview-logo" src={teacherrLogo} />
+        <img src={teacherr} />
+        <div className="titles">
+          <h1>Bienvenue sur Teacherr</h1>
+          <h2>Une application de réservation de cours de tutorat</h2>
+        </div>
+        <div className="lien-btn">
+          <Link to="/connection">
+            <button href="/connection">
+              <p>Connexion</p>
+            </button>
+          </Link>
+          <Link to="/inscription">
+            <button>
+              <p>Inscription</p>
+            </button>
+          </Link>
+        </div>
       </div>
     </body>
   );
