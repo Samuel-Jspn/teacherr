c18f8f781ab8a178bad31b86492ec53ef0061d30
sam.josephine@outlook.fr <sam.josephine@outlook.fr>
2023-06-20 11:51:04 +0200

    fix routes after merge Louis

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Connection from "./pages/connection";
 import Inscription from "./pages/inscription";
-// import Accueil from "./pages/accueil";
+import Accueil from "./pages/accueil";
 
 function App() {
   return (
     <div className="Teacherr">
         <Routes>
-          {/* <Route path="/" element={<Accueil />} /> */}
+          <Route path="/" element={<Accueil />} />
           <Route path="/auth/login" element={<Connection />} />
           <Route path="/auth/register" element={<Inscription />} />
           <Route path="/home" element={<Home />} />
