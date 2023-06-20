73b5140e64890306837a8d9705d68d2b08c339c6
sam.josephine@outlook.fr <sam.josephine@outlook.fr>
2023-06-20 16:37:08 +0200

    WIP Profile component
    Almost finished component. Need to fix icons and adapt to back

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -7,7 +7,7 @@ import Student from "./components/student/student.jsx";
 function home() {
   return (
     <>
-      <div><Navbar /></div>
+      <div className="home-div"><Navbar /></div>
       <section className="conteneur_annonces">
       <Teacher title="Professeur de maths" level="Bac +2" description="Un professeur de mathématiques est un enseignant spécialisé dans l'enseignement des concepts mathématiques aux élèves de différents niveaux scolaires. Leur rôle principal est de transmettre les connaissances mathématiques de manière claire et compréhensible, en aidant les élèves à développer une compréhension approfondie des concepts et des compétences mathématiques." />
       <Teacher title="Professeur de français" level="Bac +5" description="Test" />
--- src/pages/profil.jsx
+++ src/pages/profil.jsx
@@ -4,6 +4,9 @@ import { Link } from "react-router-dom";
 import anonymous from "./assets/anonymous.jpg";
 import Navbar from "./components/navbar/navbar.jsx";
 import { BsEyeSlash, BsEye } from "react-icons/bs";
+import { FaEdit, FaTrash } from "react-icons/fa";
+
+const password = "MotdepasseTest123";
 
 const user = [
   {
@@ -27,17 +30,39 @@ function home() {
         <Navbar />
       </div>
       <section className="profile-infos">
-        <h1>Profile Utilisateur</h1>
+        <h1>Profil Utilisateur</h1>
         <div className="profile-component">
-          <img className="profile-img" src={anonymous} />
+          <div>
+            <div className="profile-images">
+              <img className="profile-picture" src={anonymous} />
+            </div>
+          </div>
           <h1>Gérard BERTRAND</h1>
           <div className="email">
             <h1>Adresse Mail</h1>
             <p>feur.feur@feur.fr</p>
           </div>
           <div className="password">
+            <h1>Mot de Passe</h1>
+            <input type="password" className="psw" value={password} />
             {/* <input type={passwordShown ? "text" : "password"} /> */}
             {/* <button onClick={togglePassword}>{BsEye}</button> */}
+            <div className="profile-buttons">
+              <button class="edit-btn">
+                <div>
+                  <span>Éditer</span>
+                </div>
+                <div>
+                  {/* <FaEdit className="profile-icons" /> */}
+                  {/* <FontAwesomeIcon icon="fa-solid fa-pen-to-square" /> */}
+                  <i class="fa-solid fa-pen-to-square"></i>
+                </div>
+              </button>
+              <button class="delete-btn">
+                Supprimer <FaTrash />
+              </button>
+            </div>
+            {/* Ajouter div 2 boutons éditer et supprimer */}
           </div>
         </div>
       </section>
