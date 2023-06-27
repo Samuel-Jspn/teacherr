f60ccfd9f1d3e0b2f5511811ea22ae8fcec00a73
sam.josephine@outlook.fr <sam.josephine@outlook.fr>
2023-06-27 10:01:35 +0200

    added smileys on notation

--- src/pages/components/Note/note.jsx
+++ src/pages/components/Note/note.jsx
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./note.css";
+import Happy from "../../assets/happy_smiley.png";
+import Neutral from "../../assets/neutral_smiley.png";
+import Unhappy from "../../assets/unhappy_smiley.png";
 
 const RatingForm = () => {
   const [rating, setRating] = useState(0);
-  const [comment, setComment] = useState('');
+  const [comment, setComment] = useState("");
 
   const handleRatingClick = (selectedRating) => {
     setRating(selectedRating);
@@ -16,42 +19,48 @@ const RatingForm = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     // Effectuer ici l'action souhaitée avec la note (rating) et le commentaire (comment)
-    console.log('Note:', rating);
-    console.log('Commentaire:', comment);
+    console.log("Note:", rating);
+    console.log("Commentaire:", comment);
     // Réinitialiser les valeurs du formulaire
     setRating(0);
-    setComment('');
+    setComment("");
   };
 
   return (
-    <div className='note'>
-      <form onSubmit={handleSubmit} className='nForm'>
-      <h2 className='nTitre'>Formulaire de notation</h2>
+    <div className="note">
+      <form onSubmit={handleSubmit} className="nForm">
+        <h2 className="nTitre">Formulaire de notation</h2>
         <div>
-          <h1 className='title_note'>Choisissez une note :</h1>
-          <div>
+          <h1 className="title_note">Choisissez une note :</h1>
+          <div className="emojis">
             <img
-              src="star-filled.png"
+              src={Happy}
               onClick={() => handleRatingClick(1)}
-              style={{ cursor: 'pointer' }}
+              className="notation_emoji"
             />
             <img
-              src="star-filled.png"
+              src={Neutral}
               onClick={() => handleRatingClick(2)}
-              style={{ cursor: 'pointer' }}
+              className="notation_emoji"
             />
             <img
-              src="star-filled.png"
+              src={Unhappy}
               onClick={() => handleRatingClick(3)}
-              style={{ cursor: 'pointer' }}
+              className="notation_emoji"
             />
           </div>
         </div>
-        <div>
-          <h1 className='title_note'>Ajoutez un commentaire :</h1>
-          <textarea className='com' value={comment} onChange={handleCommentChange}></textarea>
+        <div className="comment">
+          <h1 className="title_note">Ajoutez un commentaire :</h1>
+          <textarea
+            className="com"
+            value={comment}
+            onChange={handleCommentChange}
+          ></textarea>
         </div>
-        <button className='btn-note' type="submit">Soumettre</button>
+        <button className="btn-note" type="submit">
+          Soumettre
+        </button>
       </form>
     </div>
   );
